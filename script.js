3d3429b6ba4e58fa489b136bb810f0e778b86c93
pereir427 <franciscopereira0227@gmail.com>
2025-04-16 19:57:28 -0300

    commit de animacoes

--- script.js
+++ script.js
@@ -1,40 +1,26 @@
 
 
+AOS.init();
+
 // Menu botão
 function menuOnClick() {
-    document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("menu-bar").classList.toggle("change");
 
-    document.getElementById("nav").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
 
-    document.getElementById("menu-bg").classList.toggle("change-bg");
-
+  document.getElementById("menu-bg").classList.toggle("change-bg");
 
 }
 
-//Maquina de escrever
-
-// function typerWrite(elemento){
-//     const textoArray = elemento.innerHTML.split('');
-//     elemento.innerHTML = ' ';
-//     textoArray.forEach(function(letra, i) {
-//         setTimeout(function() {
-//             elemento.innerHTML += letra;
-//         }, 175 * i)
-        
-//     });
-// }
-
-// const titulo = document.querySelector('.escrever');
-// typerWrite(titulo);
 
 function write(elemento) {
-    const textoArray = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    textoArray.forEach((letra, index) =>{
-      setTimeout(() => elemento.innerHTML += letra, 75 * index);
-    });
-  }
-  
-  const titulo = document.querySelector('.escrever');
-  write(titulo);
-  write(document.querySelector('.escrever-h1'));
+  const textoArray = elemento.innerHTML.split('');
+  elemento.innerHTML = '';
+  textoArray.forEach((letra, index) =>{
+    setTimeout(() => elemento.innerHTML += letra, 75 * index);
+  });
+}
+
+const titulo = document.querySelector('.escrever');
+write(titulo);
+write(document.querySelector('.escrever-h1'));
